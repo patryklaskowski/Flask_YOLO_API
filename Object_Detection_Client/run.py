6024fe5951fb75ea8823ef261392c6252f083208
Patryk IBM <patryk.jacek.laskowski@gmail.com>
2021-02-03 16:19:48 +0100

    Client updated with ip attribute. Requirements for client.

--- Object_Detection_Client/run.py
+++ Object_Detection_Client/run.py
@@ -4,6 +4,9 @@ import argparse
 from utils.client import API_Client
 
 parser = argparse.ArgumentParser()
+parser.add_argument('-ip', '--ipaddr',
+                    help='Server ip address.',
+                    default='0.0.0.0')
 parser.add_argument('-n', '--frames',
                     help='Max number of frames to send',
                     type=int,
@@ -20,12 +23,13 @@ parser.add_argument('-t', '--threshold',
                     default=0.4)
 args = parser.parse_args()
 
+ip = args.ipaddr
 source = args.frames
 weights = args.weights
 max_frames = args.source
 threshold = args.threshold
 
-client = API_Client(source)
+client = API_Client(ip, source)
 while True:
     key = input('Press Enter to post frame.')
     if key.lower()=='q':
@@ -33,5 +37,8 @@ while True:
     try:
         response, frame = client.post_frame(weights, threshold)
         print(f'status_code: {response.status_code}')
+        response_dict = response.json()
+        if 'time' in response_dict:
+            print(f'Prediction took: {response_dict["time"]} second(s).')
     except Exception as e:
         print(f'----------------\nException occured:\n--------\n{e}\n\n----------------')
--- Object_Detection_Client/utils/client.py
+++ Object_Detection_Client/utils/client.py
@@ -6,13 +6,14 @@ from .camera import Camera
 
 class API_Client:
 
-    url_structure = 'http://localhost:5000/api/detect/%s/%s'
+    url_structure = 'http://{ip}:5000/api/detect/{weights}/{threshold}'
     client_auth_code = 'success'
     print('>> API_Client Class Created!')
 
 
-    def __init__(self, source=0):
+    def __init__(self, ip='0.0.0.0', source=0):
         self.source = source
+        self.ip = ip
         self.cam = Camera(source)
         print(f'>> API_Client object id no. {id(self)} created.')
 
@@ -27,7 +28,7 @@ class API_Client:
         OUTPUT:
         response, frame
         '''
-        url = API_Client.url_structure % (weights, threshold)
+        url = API_Client.url_structure.format(ip=self.ip, weights=weights, threshold=threshold)
         frame = self.cam.get_frame()
         files = self.__create_files(frame, msg='Object_detection')
         headers = self.__create_headers(frame.shape, frame.dtype.name)
