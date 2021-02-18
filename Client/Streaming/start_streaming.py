a7cfe1e2d8bbc252a8a00a426af2d8caf7a3a9d6
Patryk Laskowski <patryk.jacek.laskowski@gmail.com>
2021-02-18 16:52:54 +0100

    Fixed streaming. For RaspberryPi.

--- Client/Streaming/start_streaming.py
+++ Client/Streaming/start_streaming.py
@@ -1,38 +1,77 @@
-# start_streaming.py
+# Get username:
+# id -un
 
+# Connect via ssh (make sure ssh is enabled)
+# ssh <username>@<ip>
+
+# On RasberryPi:
+
+# Enable ssh: https://www.raspberrypi.org/documentation/remote-access/ssh/README.md
+# sudo systemctl enable ssh
+# sudo systemctl start ssh
+
+# OpenCv dependencies:
+# sudo apt-get -y install libhdf5-dev libhdf5-serial-dev libhdf5-100
+# sudo apt-get -y install libqtgui4 libqtwebkit4 libqt4-test python3-pyqt5
+# sudo apt-get -y install libatlas-base-dev
+# sudo apt-get -y install libjasper-dev
+
+# IP address:
+# hostname -I
+
+# Add user to video usergroup:
+# sudo usermod -a -G video pi
+
+# Available cameras:
+# ls -l /dev/video*
+
+import argparse
 import cv2
-import datetime
-from flask import Flask, Response
-from imutils.video import VideoStream
 import time
+from flask import Flask, Response
 
 app = Flask(__name__)
 
-def edit(frame):
-    timestamp = datetime.datetime.now()
-    cv2.putText(frame, timestamp.strftime(
-        "%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
-        cv2.FONT_HERSHEY_SIMPLEX, 0.65, (0, 0, 255), 2)
-    return frame
-
-def generate(cam):
+class Camera:
+    
+    def __init__(self, src):
+        self.src = int(src) if len(str(src)) == 1 else str(src)
+        self.cam = cv2.VideoCapture(self.src)
+        time.sleep(1)
+        print(f'Camera instance id: {id(self)} initialized.')
+    
+    def get_frame(self, size):
+        success, frame = self.cam.read()
+        if not success: raise Exception('Cannot get frame')
+        if size > 0:
+            frame = cv2.resize(frame, (size, size))
+        return frame
+    
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-src', '--source', default='0')
+    parser.add_argument('--size', default=0, type=int)
+    parser.add_argument('-p', '--port', default=5005, type=int)
+    parser.add_argument('--debug', action='store_true')
+    args = parser.parse_args()
+    return args
+    
+    
+def generate_frames(cam):
+    global args
     while True:
-        start_time = time.perf_counter()
-        frame = cam.read()
-        frame = edit(frame)
-        _, encodedImage = cv2.imencode('.jpg', frame)
-        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
-            bytearray(encodedImage) + b'\r\n')
-
+        frame = cam.get_frame(args.size)
+        _, encoded = cv2.imencode('.jpg', frame)
+        yield(b'--frame\r\n'
+              b'Content-Type: image/jpeg\r\n\r\n' + bytearray(encoded) + b'\r\n')
+    
 @app.route('/')
-@app.route('/video')
 def video_feed():
-	return Response(generate(cam),
-        mimetype = "multipart/x-mixed-replace; boundary=frame")
+    cam = Camera(0)
+    return Response(generate_frames(cam),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
+    
 if __name__ == '__main__':
-    cam = VideoStream(src=0).start()
-    time.sleep(1)
-    app.run(host='0.0.0.0', port=5005, debug=True,
-		threaded=True, use_reloader=True)
-    cam.stop()
+    args = parse_arguments()
+    app.run(host='0.0.0.0', port=args.port, debug=args.debug, threaded=False)
